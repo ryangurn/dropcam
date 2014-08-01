039c3260b2a8b3d66055ca47fa6f107c623290a4
opfl <openthefrog@gmail.com>
2014-08-01 15:21:52 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -12,16 +12,11 @@
 	describe('Logging into Dropcam API', function() {
 		it('Should return a new user', function(done) {
 			dropcam.login(credentials.username, credentials.password, function(err, user) {
-				assert.ifError(err); 
-				done();
 
 				describe('User', function() {
 					describe('#getSessionToken', function() {
 						it('Should return the session token associated with the user', function(done) {
-							user.getSessionToken(function(err, token) {
-								assert.ifError(err);
-								done();
-							});
+							
 						});
 					});
 					describe('#addNotificationEmail', function() {
